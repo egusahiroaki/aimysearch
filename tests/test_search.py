507434dcfe9d4e693f6aa12b9806460d2ff0e1e2
egusahiroaki <egusa.hiroaki@gmail.com>
2019-07-27 00:04:44 +0900

    add tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -12,8 +12,18 @@ class TestAiMySearch(unittest.TestCase):
         """test method for run
         """
         target = "hogehoge"
-        text = "hogehogaaaaaaaaaaaaaaaahogehoge"
+        text = "hogehogfugafugapiyopihogehugepiyo"
         actual = search.AiMySearch(target, text).run()
-        expected = [{'text': 'hogehogaaa', 'index': 0, 'length': 10}, {
-            'text': 'aaahogehog', 'index': 20, 'length': 10}]
+        expected = [{'index': 0, 'length': 11, 'text': 'hogehogfuga'},
+                    {'index': 17, 'length': 14, 'text': 'yopihogehugepi'}]
+        self.assertEqual(expected, actual)
+
+    def test_run_with_(self):
+        """test method for run with high match rate
+        """
+        target = "hogehoge"
+        text = "hogehogfugafugapiyopihogehugepiyo"
+        actual = search.AiMySearch(target, text, 0, 0.8).run()
+        expected = [{'index': 0, 'length': 8, 'text': 'hogehogf'},
+                    {'index': 21, 'length': 8, 'text': 'hogehuge'}]
         self.assertEqual(expected, actual)
