e9151b9cc73c14fd4871ad6cbf2106d4e0936f2e
egusahiroaki <egusa.hiroaki@gmail.com>
2019-07-26 22:45:43 +0900

    update library info

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setuptools.setup(
     version="0.1",
     author="egusahiroaki",
     author_email="egusa.hiroaki@gmail.com",
-    description="",
+    description="fussy search for any languages.",
     long_description="",
     long_description_content_type="",
-    url="",
+    url="https://github.com/egusahiroaki/aimysearch",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7.1",
