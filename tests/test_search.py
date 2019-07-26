ec5d8e2c5cd48d9245bd793eddf30489d088b8ec
egusahiroaki <egusa.hiroaki@gmail.com>
2019-07-26 23:22:14 +0900

    add tests

---
+++ tests/test_search.py
@@ -0,0 +1,21 @@
+import unittest
+from aimysearch import search
+
+
+class TestAiMySearch(unittest.TestCase):
+    """test class of search.py
+    """
+
+    def test_run(self):
+        """test method for run
+        """
+        target = "hogehoge"
+        text = "hogehogaaaaaaaaaaaaaaaahogehoge"
+        actual = search.AiMySearch(target, text).run()
+        expected = [{'text': 'hogehogaaa', 'index': 0, 'length': 8}, {
+            'text': 'aaahogehog', 'index': 20, 'length': 8}]
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
